833ef4979201db8fd98c529d81f612ae2ab8f827
Dapinderjit Singh Dhaliwal <dhaliwaldapinder32@gmail.com>
2026-08-19 17:02:29 +0530

    Time: 2 ms (97.97%), Space: 72.4 MB (49.4%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if (bills[0] > 5)
+            return false;
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5)
+                five++;
+
+            else if (bills[i] == 10) {
+                if (five == 0)
+                    return false;
+
+                five--;
+                ten++;
+
+            } else {
+                if (five > 0 && ten > 0) {
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3){
+                    five = five - 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
