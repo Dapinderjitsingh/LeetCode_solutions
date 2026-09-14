72a63a72e3a901dec3a2ea3295886c608467483f
Dapinderjit Singh Dhaliwal <dhaliwaldapinder32@gmail.com>
2026-09-14 17:01:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.05%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,21 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n<=1) return n;
+//         return fib(n-1)+fib(n-2);
+//     }
+// }
+class Solution{
+    public int fib(int n){
+        if(n<=1) return n;
+        int prev2=0;
+        int prev1=1; 
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+
+        }
+        return curr;
+    }
+}
