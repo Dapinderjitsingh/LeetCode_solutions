f3dd4e5042c9e768c9f3afda57f697d8d3f8d507
Dapinderjit Singh Dhaliwal <dhaliwaldapinder32@gmail.com>
2026-09-21 22:26:33 +0530

    Time: 6 ms (25.29%), Space: 46.9 MB (29.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,38 +8,60 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if(lists == null || lists.length == 0) return null;
+
+//         return merge(lists, 0, lists.length - 1);
+//     }
+//     private ListNode merge(ListNode[] lists, int left, int right){
+//         if(left == right) return lists[left];
+//         int mid = (left + right) / 2;
+//         ListNode leftlist = merge(lists, left, mid);
+//         ListNode rightlist = merge(lists, mid + 1, right);
+//         return mergeTwolists(leftlist, rightlist);
+//     }
+//     private ListNode mergeTwolists(ListNode leftlist, ListNode rightlist){
+//         ListNode dummy = new ListNode(-1);
+//         ListNode curr = dummy;
+//         ListNode h1 = leftlist;
+//         ListNode h2 = rightlist;
+//         while(h1 != null && h2 != null){
+//             if(h1.val <= h2.val){
+//                 curr.next = h1;
+//                 h1 = h1.next;
+//                 curr = curr.next;
+//             }
+//             else{
+//                 curr.next = h2;
+//                 h2 = h2.next;
+//                 curr = curr.next;
+//             }
+//         }
+//         if(h1 != null) curr.next = h1;
+//         else curr.next = h2;
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length == 0) return null;
-        
-        return merge(lists, 0, lists.length - 1);
-    }
-    private ListNode merge(ListNode[] lists, int left, int right){
-        if(left == right) return lists[left];
-        int mid = (left + right) / 2;
-        ListNode leftlist = merge(lists, left, mid);
-        ListNode rightlist = merge(lists, mid + 1, right);
-        return mergeTwolists(leftlist, rightlist);
-    }
-    private ListNode mergeTwolists(ListNode leftlist, ListNode rightlist){
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-        ListNode h1 = leftlist;
-        ListNode h2 = rightlist;
-        while(h1 != null && h2 != null){
-            if(h1.val <= h2.val){
-                curr.next = h1;
-                h1 = h1.next;
-                curr = curr.next;
-            }
-            else{
-                curr.next = h2;
-                h2 = h2.next;
-                curr = curr.next;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                pq.offer(lists[i]);
             }
         }
-        if(h1 != null) curr.next = h1;
-        else curr.next = h2;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!pq.isEmpty()) {
+            ListNode current = pq.poll();
+            tail.next = current;
+            tail = tail.next;
+            if (current.next != null) {
+                pq.offer(current.next);
+            }
+        }
         return dummy.next;
     }
 }
